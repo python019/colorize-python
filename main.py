315cd053ab4260ed62f45f40d72dd132f6b7cac0
SUSYS <82611064+python019@users.noreply.github.com>
2021-08-03 00:58:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 
-prototxt_path = 'models/colorization_deploy_v2.prototxt'
-model_path = 'models/colorization_deploy_v2.caffemodel'
-karnel_path = 'models/pts_in_hull.npy'
-image_path = 'my.jpg'
+prototxt = "models/colorization_deploy_v2.prototxt"
+caffe_model = "models/colorization_release_v2.caffemodel"
+pts_npy = "models/pts_in_hull.npy"
+test_image = 'my.jpg'
 
-net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
-points = np.load(karnel_path)
+net = cv2.dnn.readNetFromCaffe(prototxt, caffe_model)
+pts = np.load(pts_npy)
+ 
+layer1 = net.getLayerId("class8_ab")
+layer2 = net.getLayerId("conv8_313_rh")
 
-points = points.transpose().reshape(2, 313, 1, 1)
-net.getLayer(net.getLayerId("class8_ab")).blobs = [points.astype(np.float32)]
-net.getLayer(net.getLayerId("conv8_313_rh")).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+pts = pts.transpose().reshape(2, 313, 1, 1)
 
-bw_image = cv2.imread(image_path)
-normalized = bw_image.astype("float32") / 255.0
-lab = cv2.cvtColor(normalized, cv2.COLOR_BGR2LAB)
+net.getLayer(layer1).blobs = [pts.astype("float32")]
 
-resized = cv2.resize(lab, (224, 224))
-L = cv2.split(resized)[0]
-L -= 50
+net.getLayer(layer2).blobs = [np.full([1, 313], 2.606, dtype="float32")]
 
-net.setInput(cv2.dnn.blobFromImage(L))
-ab = net.forward()[0, :, :, :].transpose((1,2,0))
+test_image = cv2.imread(test_image)
 
-ab = cv2.resize(ab, (bw_image.shape[1], bw_image.shape[0]))
-L = cv2.split(lab)[0]
+test_image = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
 
-colorized = np.concatenate((L[:,:,:,np.newaxis], ab), axis=2)
-colorized = np.cvtColor(colorized, cv2.COLOR_LAB2BGR)
-colorized = (255.0 * colorized).astype("uint8")
+test_image = cv2.cvtColor(test_image, cv2.COLOR_GRAY2RGB)
 
-cv2.imshow("BW Image", bw_image)
-cv2.imshow("Colorized", colorized)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(test_image)
+plt.show()
+
